00fa01814b47cf49dc717ecfaf9efec65c837d54
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-08 18:35:48 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.owner = [
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['5493536568522', '59176181985']
+global.prems = ['5493536568522', '5493625494354']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
