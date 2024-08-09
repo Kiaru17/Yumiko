188f50b66f2ba181be0bf6355dfd5e62722db6da
Kiaru17 <kiaralarrondo67@gmail.com>
2024-08-09 08:57:20 -0300

    Actualizar owner-delprem.js

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false
     else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🌸 ${mssg.noMention} 🌸\n\n🌸 *${mssg.example}* 🌸:\n${usedPrefix + command} @${m.sender.split`@`[0]}`, null, { mentions: [m.sender] })
-    if (!(who in global.db.data.users)) throw `🌸 ${mssg.userDb} 🌸`
-    if (user.prem === false) throw `🌸 El Usuario no es Premium 🌸`
+    if (!who) return m.reply(`🌸 ⃟᮪〭۬̇〬⃟˖⸱ೇ ${mssg.noMention} 🌸 ⃟᮪〭۬̇〬⃟˖⸱ೇ\n\n🌸 ⃟᮪〭۬̇〬⃟˖⸱ೇ *${mssg.example}* 🌸 ⃟᮪〭۬̇〬⃟˖⸱ೇ:\n${usedPrefix + command} @${m.sender.split`@`[0]}`, null, { mentions: [m.sender] })
+    if (!(who in global.db.data.users)) throw `⸵░⃟🌸 ${mssg.userDb} ⸵░⃟🌸`
+    if (user.prem === false) throw `❍⃕⃟᎒⃟̀🌸 El Usuario no es Premium ❍⃕⃟᎒⃟̀🌸`
     user.prem = false
     user.premiumTime = 0
     m.reply(`✅ Premium removido \n\n@${who.split('@')[0]} ya no eres premium`, null, { mentions: [who] })
