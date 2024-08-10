3bfb601ca35bc01a3fb17ca0daebfdc1b29c2c6f
Kiaru17 <kiaralarrondo67@gmail.com>
2024-08-10 10:08:11 -0300

    Actualizar dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -2,7 +2,7 @@
 import yts from 'yt-search';
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     if (!text) throw `🌸 ${mssg.example} 🌸 *${usedPrefix + command}* Lil Peep hate my life`;
-    m.react('📀');
+    m.react('🫦');
     
     let result = await yts(text);
     let ytres = result.videos;
@@ -15,22 +15,22 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
             title: `${index}┃ ${v.title}`,
             rows: [
                 {
-                    header: '💿 MP3',
+                    header: ' ⋆⃟ۣۜ᭪🌸➣ MP3',
                     title: "",
-                    description: `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`, 
+                    description: `▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.duration}:* ${v.timestamp}\n▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.views}:* ${v.views}\n▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.title}* : ${v.title}\n▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.aploud}:* ${v.ago}\n`, 
                     id: `${usedPrefix}fgmp3 ${v.url}`
                 },
                 {
-                    header: "📀 MP4",
+                    header: " ⋆⃟ۣۜ᭪🌸➣ MP4",
                     title: "" ,
-                    description: `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`, 
+                    description: `▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.duration}:* ${v.timestamp}\n▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.views}:* ${v.views}\n▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.title}* : ${v.title}\n▢  ⋆⃟ۣۜ᭪🌸➣ *${mssg.aploud}:* ${v.ago}\n`, 
                     id: `${usedPrefix}fgmp4 ${v.url}`
                 }
             ]
         });
     }
 
-    await conn.sendListB(m.chat, '🌸 *YUMIKO BOT* 🌸 ', `\n 📀 Resultados de:\n *${text}*`, `OPCIONES`, ytres[0].image, listSections, m);
+    await conn.sendListB(m.chat, ' ░⃟🌸𝐃𝐄𝐀𝚻𝐇 𝐍𝐎𝚻𝐄 𝐁𝐎𝐓░⃟🌸 ', `\n ♥⃟᮪〭۬̇〬⃟˖ꪶ Resultados de:\n *${text}*`, `OPCIONES`, ytres[0].image, listSections, m);
 };
 
 handler.help = ['play2']
