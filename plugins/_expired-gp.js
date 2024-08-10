91d7f9ff69cdd57477eb95a8e362ed065d968d86
Kiaru17 <kiaralarrondo67@gmail.com>
2024-08-10 09:43:57 -0300

    Actualizar _expired-gp.js

--- plugins/_expired-gp.js
+++ plugins/_expired-gp.js
@@ -6,7 +6,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `🌸 Adiós shavales 🌸 *${this.user.name}* saldrá del grupo \n\nfinalizo su alquiler`)
+        await this.reply(m.chat, `✧͢⃟ᤢ🌸 Adiós Pendejes ✧͢⃟ᤢ🌸 *${this.user.name}* saldrá del grupo \n\nfinalizo su alquiler`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
