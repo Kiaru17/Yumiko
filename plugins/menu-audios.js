db4e9fcb22501344035fc13e860110d68c182580
Kiaru17 <kiaralarrondo67@gmail.com>
2026-04-03 22:29:36 -0300

    Actualizar menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -1,4 +1,3 @@
-
 let handler = async function (m, { conn, text, usedPrefix }) {
 
     m.react('🗣️')
@@ -6,28 +5,28 @@ let handler = async function (m, { conn, text, usedPrefix }) {
 let m2 = `
 ░⃟🌸𝐃𝐄𝐀𝚻𝐇 𝐍𝐎𝚻𝐄 𝐁𝐎𝐓░⃟🌸
 
-╔════════⫹♥⃟᮪〭۬̇〬⃟˖ꪶ⫺════════╗
+╔════════⫹♥⃟᮪〭۬̇〬⃟˖ꪶ⫺════════╗
 ║    𝐌𝐄𝐍𝐔 メ 𝐀𝐔𝐃𝐈𝐎𝐒
-╠════════♥⃟᮪〭۬̇〬⃟˖ꪶ════════╝
+╠════════♥⃟᮪〭۬̇〬⃟˖ꪶ════════╝
   ━━━━━━━━⧔❍⃕⃟🌸⧕━━━━━━━
-┃╭━─━─━──≪ ♥⃟᮪〭۬̇〬⃟˖ꪶ ≫─━──━─━╮
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘢𝘳𝘢
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘧𝘪𝘯𝘰𝘴 𝘴𝘦𝘯̃𝘰𝘳𝘦𝘴
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘣𝘢𝘯̃𝘢𝘵𝘦
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘣𝘶𝘦𝘯𝘰𝘴 𝘥𝘪𝘢𝘴
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘥𝘪𝘢𝘨𝘯𝘰𝘴𝘵𝘪𝘤𝘰
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘦𝘭𝘮𝘰
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘱𝘶𝘵𝘰
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘨𝘦𝘮𝘪𝘥𝘰𝘴
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘱𝘰𝘣𝘳𝘦 𝘱𝘦𝘳𝘳𝘢
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘮𝘢𝘯𝘤𝘰
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘮𝘪𝘢𝘶
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘵𝘳𝘢𝘬𝘢
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘵𝘶𝘳𝘪𝘱
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘩𝘪𝘯𝘮𝘰 𝘱𝘰𝘵𝘢𝘹𝘪𝘦
-┃  ⋆⃟ۣۜ᭪🌸➣ .𝘢
-┃  ⋆⃟ۣۜ᭪🌸➣ .:𝘤
-┃╰━─━──━─≪ ♥⃟᮪〭۬̇〬⃟˖ꪶ ≫─━──━─━╯
+┃╭━─━─━──≪ ♥⃟᮪〭۬̇〬⃟˖ꪶ ≫─━──━─━╮
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘢𝘳𝘢
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘧𝘪𝘯𝘰𝘴 𝘴𝘦𝘯̃𝘰𝘳𝘦𝘴
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘣𝘢𝘯̃𝘢𝘵𝘦
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘣𝘶𝘦𝘯𝘰𝘴 𝘥𝘪𝘢𝘴
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘥𝘪𝘢𝘨𝘯𝘰𝘴𝘵𝘪𝘤𝘰
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘦𝘭𝘮𝘰
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘱𝘶𝘵𝘰
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘨𝘦𝘮𝘪𝘥𝘰𝘴
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘱𝘰𝘣𝘳𝘦 𝘱𝘦𝘳𝘳𝘢
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘮𝘢𝘯𝘤𝘰
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘮𝘪𝘢𝘶
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘵𝘳𝘢𝘬𝘢
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘵𝘶𝘳𝘪𝘱
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘩𝘪𝘯𝘮𝘰 𝘱𝘰𝘵𝘢𝘹𝘪𝘦
+┃  ⋆⃟ۣۜ᭪🌸➣ .𝘢
+┃  ⋆⃟ۣۜ᭪🌸➣ .:𝘤
+┃╰━─━──━─≪ ♥⃟᮪〭۬̇〬⃟˖ꪶ ≫─━──━─━╯
   ━━━━━━━━⧔❍⃕⃟🌸⧕━━━━━━━
 `
     let pp = 'https://i.ibb.co/pnTYkw8/file.jpg' 
@@ -40,20 +39,15 @@ global.fcontact = {
         },
         message: {
             contactMessage: {
-                displayName: `\n ░⃟🌸𝐃𝐄𝐀𝚻𝐇 𝐍𝐎𝚻𝐄 𝐁𝐎𝐓░⃟🌸 \n
-𝐋𝐀 𝐌𝐄𝐉𝐎𝐑 𝐑𝐄𝐘𝐍𝐀`,
+                displayName: `\n ░⃟🌸𝐃𝐄𝐀𝚻𝐇 𝐍𝐎𝚻𝐄 𝐁𝐎𝐓░⃟🌸 \n𝐋𝐀 𝐌𝐄𝐉𝐎𝐑 𝐑𝐄𝐘𝐍𝐀`,
                 vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:xd\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
             },
         },
     };
 
-    await conn.reply(m.chat, '𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 𝐌𝐄𝐍𝐔♥⃟᮪〭۬̇〬⃟˖ꪶ', fcontact);
-    /*conn.sendButton(m.chat, m2, mssg.ig, pp, [
-      ['⏍ Info', `${usedPrefix}botinfo`],
-      ['⌬ Grupos', `${usedPrefix}gpdylux`]
-    ],m, rpyt)*/
-    conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rcanal)
-
+    await conn.reply(m.chat, '𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 𝐌𝐄𝐍𝐔♥⃟᮪〭۬̇〬⃟˖ꪶ', global.fcontact);
+    // Alternativa: usa sendFile en lugar de reply si prefieres
+    await conn.sendFile(m.chat, pp, 'menu.jpg', m2, m)
 }
 
 handler.help = ['menuaudios']
