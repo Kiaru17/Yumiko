e77647064c2abb0dc7eb2e9a3b21d095f241dc47
Kiaru17 <kiaralarrondo67@gmail.com>
2026-04-03 23:11:53 -0300

    Actualizar Imagen-pikachu.js

--- plugins/Imagen-pikachu.js
+++ plugins/Imagen-pikachu.js
@@ -1,17 +1,4 @@
-let handler = async (m, {conn, command}) => {
-  let url = pack[Math.floor(Math.random() * pack.length)];
-    await conn.sendFile( 
-     m.chat, 
-     url, 
-     "gimage.jpg", 
-     ` 
- Pikachu`.trim(), m)
-};
-handler.help = ["pikachu"];
-handler.tags = ["internet"];
-handler.command = /^(pikachu)$/i;
-export default handler;
-
+// Definir primero las imágenes
 global.pack = [
   "https://telegra.ph/file/5a382d2ee0f08306e9970.jpg",
   "https://telegra.ph/file/ccdcd6a92c4f6b1c11a00.jpg",
@@ -20,4 +7,20 @@ global.pack = [
   "https://telegra.ph/file/1fbb857f22b30578ae1b6.jpg",
 ];
 
+let handler = async (m, { conn, command }) => {
+  let url = global.pack[Math.floor(Math.random() * global.pack.length)];
+  await conn.sendFile(
+    m.chat,
+    url,
+    "pikachu.jpg",
+    `🌸 *PIKACHU ALEATORIO* 🌸`,
+    m
+  );
+};
+
+handler.help = ["pikachu"];
+handler.tags = ["internet"];
+handler.command = /^(pikachu)$/i;
 handler.limit = 3;
+
+export default handler;
