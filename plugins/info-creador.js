7de4bdd33551e66608314688e02173ad443bc8d2
Kiaru17 <kiaralarrondo67@gmail.com>
2026-04-03 23:18:07 -0300

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,21 +1,21 @@
 import fetch from 'node-fetch'
+
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-m.react('⭐')
-
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = await conn.getName(who)
-let edtr = `@${m.sender.split`@`[0]}`
-
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;𝐤𝐢𝐚𝐫𝐚 ✰\nNICKNAME:𝐤𝐢𝐚𝐫𝐚🌸\nORG:𝐤𝐢𝐚𝐫𝐚 ✰\nTITLE:soft\nitem1.TEL;waid=5493625494354:+5493625494354\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/karim-off/GenesisBot-Pro1\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: agasistencia2@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇦🇷 Argentina;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date: 09-12-2007\nEND:VCARD`
-const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: packname, contacts: [{ vcard }] }}, { quoted: fkontak })
-let caption = ` 𝐇𝐎𝐋𝐀 ${edtr},⸵░⃟🌸 𝐄𝐒𝐓𝐄 𝐄𝐒 𝐄𝐋 𝐍𝐔𝐌𝐄𝐑𝐎 𝐃𝐄 𝐌𝐈 𝐂𝐑𝐄𝐀𝐃𝐎𝐑 ⸵░⃟🌸`
-    await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
-
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['main']
-handler.command = /^(owner|creator|creador|dueño)$/i
-
-export default handler
+  m.react('⭐')
+  
+  let fkontak = { 
+    "key": { 
+      "participants": "0@s.whatsapp.net", 
+      "remoteJid": "status@broadcast", 
+      "fromMe": false, 
+      "id": "Halo" 
+    }, 
+    "message": { 
+      "contactMessage": { 
+        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Bot\nFN:Yumiko Bot\nTEL;type=CELL;type=WHATSAPP;waid=5493625494354:+5493625494354\nEND:VCARD` 
+      } 
+    } 
+  }
+  
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let name = await
