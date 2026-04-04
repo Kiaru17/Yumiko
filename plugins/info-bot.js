b4ffa9429fcd5279bf1fdca3b091e271c4386113
Kiaru17 <kiaralarrondo67@gmail.com>
2026-04-03 23:17:02 -0300

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,28 +1,23 @@
 import fs from 'fs';
+
 const handler = (m) => m;
+
 handler.all = async function(m) {
- // const vn = './src/mp3/bot.mp3';
   const chat = global.db.data.chats[m.chat];
-  //const s = seconds: '4556'
-  // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
-  // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
+  
+  // Responder a "bot"
   if (/^bot$/i.test(m.text) && !chat.isBanned) {
-    conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`𝐇𝐨𝐥𝐚 𝐯𝐯🫦 como estas?`);
-import fs from 'fs';
-const handler = (m) => m;
-handler.all = async function(m) {
- // const vn = './src/mp3/bot.mp3';
-  const chat = global.db.data.chats[m.chat];
-  //const s = seconds: '4556'
-  // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
-  // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
-if (/^como estás?$/i.test(.text) && !chat.isBanned){m.text) && !chat.isBanned) {
-    conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`bien y vos?`);
-    m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
-    // conn.sendFile(m.chat, vn, 'HolaXiaBot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
+    global.conn.sendPresenceUpdate('recording', m.chat);
+    await m.reply(`𝐇𝐨𝐥𝐚 𝐯𝐯🫦 ¿Cómo estás?`);
   }
+  
+  // Responder a "cómo estás"
+  if (/^(cómo estás|como estas|hola bot)$/i.test(m.text) && !chat.isBanned) {
+    global.conn.sendPresenceUpdate('recording', m.chat);
+    await m.reply(`¡Bien y vos! 😊`);
+  }
+  
   return !0;
 };
+
 export default handler;
